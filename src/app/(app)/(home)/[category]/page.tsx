71bca1e5479d039c88388111ebb80d3af90607f8
Saharul Sifat <126339980+MernSpace@users.noreply.github.com>
2025-08-01 14:51:29 +0600

    11 products

--- src/app/(app)/(home)/[category]/page.tsx
+++ src/app/(app)/(home)/[category]/page.tsx
@@ -1,3 +1,8 @@
+import { ProductList, ProductListSkeleton } from "@/modules/products/ui/components/product-list";
+import { trpc, getQueryClient } from "@/trpc/server";
+import { dehydrate, HydrationBoundary } from "@tanstack/react-query";
+import { Suspense } from "react";
+
 interface Props {
     params: Promise<{
         category: string;
@@ -5,10 +10,17 @@ interface Props {
 }
 const page = async ({ params }: Props) => {
     const { category } = await params;
+    const queryClient = getQueryClient();
+    void queryClient.prefetchQuery(trpc.products.getMany.queryOptions({
+        category
+    }))
+
     return (
-        <div>
-            Category {category}
-        </div>
+        <HydrationBoundary state={dehydrate(queryClient)}>
+            <Suspense fallback={<ProductListSkeleton />}>
+                <ProductList category={category} />
+            </Suspense>
+        </HydrationBoundary>
     )
 }
 
