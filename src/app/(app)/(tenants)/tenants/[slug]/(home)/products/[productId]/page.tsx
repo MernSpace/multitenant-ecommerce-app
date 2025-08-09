638ef92c9b6166950c11aa4c4972dcd53f356f2a
Saharul Sifat <126339980+MernSpace@users.noreply.github.com>
2025-08-09 09:33:37 +0600

    product page ui done

---
+++ src/app/(app)/(tenants)/tenants/[slug]/(home)/products/[productId]/page.tsx
@@ -0,0 +1,22 @@
+import { getQueryClient, trpc } from "@/trpc/server";
+import { dehydrate, HydrationBoundary } from "@tanstack/react-query";
+import { ProductView } from "@/modules/products/ui/views/product-view";
+
+interface Props {
+    params: Promise<{ productId: string; slug: string }>
+}
+
+const Page = async ({ params }: Props) => {
+    const { productId, slug } = await params;
+    const queryClient = getQueryClient()
+    void queryClient.prefetchQuery(trpc.products.getOne.queryOptions({
+        id: productId
+    }))
+    return (
+        <HydrationBoundary state={dehydrate(queryClient)}>
+            <ProductView productId={productId} tenanatSlug={slug} />
+        </HydrationBoundary>
+    )
+}
+
+export default Page
