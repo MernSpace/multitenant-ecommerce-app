77ebe53d711f4f44f6a4b3358ef4069000642935
Saharul Sifat <126339980+MernSpace@users.noreply.github.com>
2025-08-11 10:50:39 +0600

    18 cart and checkout

--- src/modules/tenants/ui/components/navbar.tsx
+++ src/modules/tenants/ui/components/navbar.tsx
@@ -5,6 +5,27 @@ import { useSuspenseQuery } from "@tanstack/react-query"
 import Image from "next/image"
 import Link from "next/link"
 import { generateTenantURL } from "../../../../lib/utils"
+// import { CheckoutButton } from "@/modules/checkout/ui/components/checkout-button"
+import dynamic from "next/dynamic"
+import { Button } from "@/components/ui/button"
+import { ShoppingCartIcon } from "lucide-react"
+
+
+const CheckoutButton = dynamic(
+    () => import("@/modules/checkout/ui/components/checkout-button").then(
+        (mod) => mod.CheckoutButton
+    ),
+    {
+        ssr: false,
+        loading: () => <Button
+            disabled
+            className="bg-white"
+        ><ShoppingCartIcon className="text-black" /></Button>
+    }
+)
+
+
+
 
 interface Props {
     slug: string
@@ -30,6 +51,10 @@ export const Navbar = ({
                     <p className="text-2xl">{data.name}</p>
 
                 </Link>
+                <CheckoutButton
+                    tenantSlug={slug}
+                    hideIfEmpty
+                />
             </div>
         </nav>
     )
@@ -41,6 +66,10 @@ export const NavbarSkeleton = () => {
         <nav className="h-20 border-b font-medium bg-white">
             <div className="max-w-(--breakpoint-xl) mx-auto flex justify-between items-center h-full px-4 lg:px-12">
                 <div />
+                <Button
+                    disabled
+                    className="bg-white"
+                ><ShoppingCartIcon className="text-black" /></Button>
             </div>
         </nav>
     )
