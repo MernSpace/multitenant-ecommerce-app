e8fef2134c8ed002ac22e0737098cac54921313e
mernspace <sifat355y@gmail.com>
2025-08-27 15:34:55 +0600

    stripe api version change

--- src/lib/stripe.ts
+++ src/lib/stripe.ts
@@ -3,6 +3,6 @@ import Stripe from "stripe"
 
 
 export const stripe = new Stripe(process.env.STRIP_SECRET_KEY!, {
-    apiVersion: "2025-03-31.basil",
+    apiVersion: "2025-07-30.basil",
     typescript: true
 })
